735f09fb937ce4df7de3ca79a4248acc0e7d2b9e
Lukas Fröhlich <lukasfro@ethz.ch>
2021-03-12 15:47:05 +0100

    Move source code to root dir

--- make_source.sh
+++
@@ -1,5 +0,0 @@
-catkin_make_isolated
-FILE="devel_isolated/setup.bash"
-echo "Sourcing the setup.bash: ${FILE}"
-source ${FILE}
-echo "?! Did you recall to use '. make_source.sh' or 'source make_source.sh' ?!"
