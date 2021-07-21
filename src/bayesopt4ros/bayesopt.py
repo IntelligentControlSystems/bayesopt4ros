Fix input normalization

src/bayesopt4ros/bayesopt.py
@@ -276,13 +276,11 @@ class BayesianOptimization(object):
         self._optimize_model()
 
     def _initialize_model(self, x, y) -> GPyTorchModel:
-        # Note: the default values from BoTorch are quite good
-        unit_cube = torch.stack((torch.zeros(self.input_dim), torch.ones(self.input_dim)))
         gp = SingleTaskGP(
             train_X=x,
             train_Y=y,
             outcome_transform=Standardize(m=1),  # zero mean, unit variance
-            input_transform=Normalize(d=self.input_dim, bounds=unit_cube),
+            input_transform=Normalize(d=self.input_dim, bounds=self.bounds),
         )
         return gp
 

src/bayesopt4ros/contextual_bayesopt.py
@@ -183,12 +183,11 @@ class ContextualBayesianOptimization(BayesianOptimization):
         # Joint kernel is constructed via multiplication
         covar_module = ScaleKernel(k0 * k1, outputscale_prior=GammaPrior(2.0, 0.15))
 
-        unit_cube = torch.stack((torch.zeros(self.joint_dim), torch.ones(self.joint_dim)))
         gp = SingleTaskGP(
             train_X=x,
             train_Y=y,
             outcome_transform=Standardize(m=1),
-            input_transform=Normalize(d=self.joint_dim, bounds=unit_cube),
+            input_transform=Normalize(d=self.joint_dim, bounds=self.joint_bounds),
             covar_module=covar_module,
         )
         return gp